7e201c37e6bae1cd8c29b551c2f5925dae759bb2
Numsik <honzik.klusek.myto@gmail.com>
2025-09-16 16:10:32 +0200

    first game js ready to use

--- games/guess.js
+++ games/guess.js
@@ -1,8 +1,34 @@
 let min = 0;
-let max = 10;
+let max = 0;
 let typ = 0;
 let pcmove = 0;
 let guesses = 0;
+
+
+const showyouinput = document.querySelector('.typewihbtn');
+
+function showinput(){
+    showyouinput.style.display = "block"
+}
+
+document.querySelector('.easymode').addEventListener('click', () =>{
+    max = 10;
+    PcMove()
+    showinput()
+});
+document.querySelector('.mediummode').addEventListener('click', () =>{
+    max = 20;
+    PcMove()
+    showinput()
+});
+document.querySelector('.hardmode').addEventListener('click', () =>{
+    max = 50;
+    PcMove()
+    showinput()
+});
+document.querySelector('.resetgame').addEventListener('click', () =>{
+    showyouinput.style.display = "none"
+})
 
 
 const mininpt = document.querySelector('.min');
@@ -16,21 +42,23 @@ const typinpt = document.querySelector('.typ');
 
 function gettyp(){
     typ = Number(typinpt.value);
-    console.log(typ)
 }
 document.querySelector('.SendValues').addEventListener('click', () =>{
     gettyp();
     isittrue(typ, pcmove);
 })
 function PcMove(){
+    if (max === 0){
+        console.log('Before select dificulty');
+        return;
+    }
     let random = (Math.random() * (max - min) + min);
     let Move = Math.round(random)
     pcmove = Number(Move)
-    console.log(pcmove)
-    
+     
 }
 function isittrue(player, pc){
-    if (guesses < 8){
+    if (guesses < 7){
         if (player > pc){
         console.log('too high try again')
         guesses ++;
