8f2c1b9ee011e6a0896ce25855c0b66d2f739159
Numsik <honzik.klusek.myto@gmail.com>
2025-09-16 15:08:53 +0200

    first game getting some things done

--- games/guess.js
+++ games/guess.js
@@ -1,7 +1,49 @@
 let min = 0;
-let max = 100;
-export function guess(){
+let max = 10;
+let typ = 0;
+let pcmove = 0;
+let guesses = 0;
+
+
+const mininpt = document.querySelector('.min');
+const maxinpt = document.querySelector('.max');
+
+
+
+const typinpt = document.querySelector('.typ');
+
+
+
+function gettyp(){
+    typ = Number(typinpt.value);
+    console.log(typ)
+}
+document.querySelector('.SendValues').addEventListener('click', () =>{
+    gettyp();
+    isittrue(typ, pcmove);
+})
+function PcMove(){
     let random = (Math.random() * (max - min) + min);
-    console.log(Math.round(random));
+    let Move = Math.round(random)
+    pcmove = Number(Move)
+    console.log(pcmove)
     
 }
+function isittrue(player, pc){
+    if (guesses < 8){
+        if (player > pc){
+        console.log('too high try again')
+        guesses ++;
+        }else if(player < pc){
+        console.log("to low try again")
+        guesses ++;
+        }else{
+        console.log("you are right")
+        }
+    }else{
+        console.log('you lost')
+    }
+
+
+}
+PcMove();
