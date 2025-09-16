f7fb727a297a5b6dfa42c7abe6bd25f4d415e904
Numsik <honzik.klusek.myto@gmail.com>
2025-09-16 17:11:20 +0200

    adding cs to first game

--- games/guess.js
+++ games/guess.js
@@ -1,77 +1,80 @@
-let min = 0;
+let min = 1;
 let max = 0;
 let typ = 0;
 let pcmove = 0;
 let guesses = 0;
 
-
 const showyouinput = document.querySelector('.typewihbtn');
-
-function showinput(){
-    showyouinput.style.display = "block"
-}
-
-document.querySelector('.easymode').addEventListener('click', () =>{
-    max = 10;
-    PcMove()
-    showinput()
-});
-document.querySelector('.mediummode').addEventListener('click', () =>{
-    max = 20;
-    PcMove()
-    showinput()
-});
-document.querySelector('.hardmode').addEventListener('click', () =>{
-    max = 50;
-    PcMove()
-    showinput()
-});
-document.querySelector('.resetgame').addEventListener('click', () =>{
-    showyouinput.style.display = "none"
-})
-
-
-const mininpt = document.querySelector('.min');
-const maxinpt = document.querySelector('.max');
-
-
-
+const modeselection = document.querySelector('.modeselection');
+const textselect = document.querySelector('.textselect');
+const whatyouhadtodo = document.querySelector('.whatyouhadtodo');
+const winloss = document.querySelector('.winloss');
 const typinpt = document.querySelector('.typ');
 
+function showinput() {
+    showyouinput.style.display = "block";
+    modeselection.style.display = "none";
+    textselect.style.display = "none";
+}
 
+function PcMove() {
+    if (max === 0) return;
+    let random = Math.random() * (max - min) + min;
+    pcmove = Math.round(random);
+    console.log(pcmove);
+}
 
-function gettyp(){
+function gettyp() {
     typ = Number(typinpt.value);
 }
-document.querySelector('.SendValues').addEventListener('click', () =>{
+
+function isittrue(player, pc) {
+    if (guesses < 7) {
+        if (player > pc) {
+            whatyouhadtodo.textContent = `Too high, try again`;
+            guesses++;
+        } else if (player < pc) {
+            whatyouhadtodo.textContent = `Too low, try again`;
+            guesses++;
+        } else {
+            winloss.textContent = `You win`;
+            whatyouhadtodo.textContent = '';
+        }
+    } else {
+        winloss.textContent = `You lost`;
+        whatyouhadtodo.textContent = '';
+    }
+}
+
+document.querySelector('.easymode').addEventListener('click', () => {
+    max = 10;
+    PcMove();
+    showinput();
+});
+
+document.querySelector('.mediummode').addEventListener('click', () => {
+    max = 20;
+    PcMove();
+    showinput();
+});
+
+document.querySelector('.hardmode').addEventListener('click', () => {
+    max = 50;
+    PcMove();
+    showinput();
+});
+
+document.querySelector('.resetgame').addEventListener('click', () => {
+    showyouinput.style.display = "none";
+    modeselection.style.display = "block";
+    textselect.style.display = "block";
+    guesses = 0;
+    whatyouhadtodo.textContent = '';
+    winloss.textContent = '';
+});
+
+document.querySelector('.SendValues').addEventListener('click', () => {
     gettyp();
     isittrue(typ, pcmove);
-})
-function PcMove(){
-    if (max === 0){
-        console.log('Before select dificulty');
-        return;
-    }
-    let random = (Math.random() * (max - min) + min);
-    let Move = Math.round(random)
-    pcmove = Number(Move)
-     
-}
-function isittrue(player, pc){
-    if (guesses < 7){
-        if (player > pc){
-        console.log('too high try again')
-        guesses ++;
-        }else if(player < pc){
-        console.log("to low try again")
-        guesses ++;
-        }else{
-        console.log("you are right")
-        }
-    }else{
-        console.log('you lost')
-    }
-
-
-}
-PcMove();
+    typinpt.value = ''
+});
