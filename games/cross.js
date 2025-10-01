95110de360c651fa1d39f72ada371190416a9019
Numsik <honzik.klusek.myto@gmail.com>
2025-10-01 11:51:56 +0200

    third things in cross

--- games/cross.js
+++ games/cross.js
@@ -1,26 +1,55 @@
-
 const home = document.querySelector('.home');
-home.addEventListener('click', () =>{
-    window.location.href = "../index.html"
+home.addEventListener('click', () => {
+  window.location.href = "../index.html";
 });
 
-let crossboard = ["", "","",
-                    "","","",
-                    "","",""]
+let crossboard = ["", "", "", "", "", "", "", "", ""];
 
 const boardelement = document.querySelector('.board');
+const resetgame = document.querySelector('.resetgame');
 
-
-
-function renderboard(){
-    boardelement.innerHTML = "";
-    crossboard.forEach((cell, i) =>{
-        const div = document.createElement("div");
-        div.classList.add("cell");
-        div.textContent = cell;
-        div.addEventListener('click', () =>{
-            makeamove();
-        })
-        boardelement.appendChild(div)
-    })
+resetgame.addEventListener('click', () =>{
+    crossboard = ["", "", "", "", "", "", "", "", ""];
+})
+function renderboard() {
+  boardelement.innerHTML = "";
+  crossboard.forEach((cell, i) => {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    div.textContent = cell;
+    div.addEventListener('click', () => {
+      makeamove(i);
+      renderboard();
+      checkwin();
+    });
+    boardelement.appendChild(div);
+  });
 }
+
+let currentplayer = "X";
+
+function makeamove(index) {
+  if (crossboard[index] === "") {
+    crossboard[index] = currentplayer;
+    currentplayer = currentplayer === "X" ? "O" : "X";
+  }
+}
+
+function checkwin() {
+  const wins = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  for (let combo of wins) {
+    const [a, b, c] = combo;
+    if (crossboard[a] && crossboard[a] === crossboard[b] && crossboard[a] === crossboard[c]) {
+      alert(`${crossboard[a]} vyhrál!`);
+      return;
+    }
+  }
+}
+
+
+renderboard();
