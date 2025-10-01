1e6c1bd4b101285dc44ab753393683e5c73c3494
Numsik <honzik.klusek.myto@gmail.com>
2025-10-01 11:42:10 +0200

    first things in cross

--- games/cross.js
+++ games/cross.js
@@ -1,7 +1,26 @@
 
-
-
 const home = document.querySelector('.home');
 home.addEventListener('click', () =>{
     window.location.href = "../index.html"
 });
+
+let crossboard = ["", "","",
+                    "","","",
+                    "","",""]
+
+const boardelement = document.querySelector('.board');
+
+
+
+function renderboard(){
+    boardelement.innerHTML = "";
+    crossboard.forEach((cell, i) =>{
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        div.textContent = cell;
+        div.addEventListener('click', () =>{
+            makeamove();
+        })
+        boardelement.appendChild(div)
+    })
+}
